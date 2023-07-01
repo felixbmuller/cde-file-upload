2619becd020d702a1bbfadc371e94d70b008b32a
Lars Esser <mail@lars-esser.de>
2023-07-01 22:16:52 +0200

    main: refactor functions

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from functools import wraps
-import os
 from ftplib import FTP
 import tempfile
 import logging
+from typing import Tuple, List
+import pathlib
 
 from flask import Flask, render_template, request, Response
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures.file_storage import FileStorage
 
 from secret import FTP_HOST, FTP_PORT
 
@@ -14,17 +16,22 @@ app = Flask(__name__)
 logging.basicConfig(filename='cde_file_upload.log', encoding='utf-8', level=logging.DEBUG)
 
 
-def check_auth(username, password):
-    """This function is called to check if a username /
-    password combination is valid.
-    """
+def get_credentials() -> Tuple[str, str]:
+    """Extract credentials from request state."""
+    if request.authorization:
+        return request.authorization.username, request.authorization.password
+    return request.values.get("user", ""), request.values.get("password", "")
+
+
+def check_auth(username: str, password: str) -> bool:
+    """Check authorization with username/password against the ftp server."""
     ftp = FTP()
 
     try:
         ftp.connect(FTP_HOST, FTP_PORT)
         ftp.login(username, password)
         ftp.quit()
-    except Exception as e:
+    except Exception:
         ftp.quit()
         return False
     else:
@@ -40,52 +47,54 @@ def authenticate():
 
 
 def requires_auth(f):
+    """Decorate an endpoint which requires authorization."""
     @wraps(f)
     def decorated(*args, **kwargs):
-        auth = request.authorization
-
-        if auth:
-            username = auth.username
-            password = auth.password
-        else:
-            username = request.values.get("user", "")
-            password = request.values.get("password", "")
-        
-        if not check_auth(username, password):
+        if not check_auth(*get_credentials()):
             return authenticate()
         return f(*args, **kwargs)
     return decorated
 
 
-def chdir(ftp, dir): 
-    if directory_exists(ftp, dir) is False: # (or negate, whatever you prefer for readability)
-        ftp.mkd(dir)
-    ftp.cwd(dir)
-
-
-# Check if directory exists (in current location)
-def directory_exists(ftp, dir):
-    filelist = []
-    ftp.retrlines('LIST',filelist.append)
+def is_directory(ftp: FTP, directory: str):
+    """Check if this directory exists in the current ftp folder."""
+    filelist: List[str] = []
+    # execute 'ls -l' on the ftp server
+    ftp.retrlines('LIST', filelist.append)
     for f in filelist:
-        if f.split()[-1] == dir and f.upper().startswith('D'):
-            return True
+        # one line looks like
+        # drwxrwxr-x 2 2023_PfingstAkademie akademien 0 Jun 7 22:45 DirectoryName
+        bits = f.split()[0]
+        name = f.split()[-1]
+        if name == directory:
+            if bits.upper().startswith('D'):
+                return True
+            raise ValueError(f"Can not create directory {name}: File exists.")
     return False
 
 
-def upload_file_to_ftp(ftp, file_path, filename):
+def store_file(directory: pathlib.Path, file: FileStorage) -> pathlib.Path:
+    """Save the file in the directory on the current hard-drive."""
+    name = secure_filename(file.filename)
+    path = directory / name
 
-    with open(file_path, 'rb') as file:
-        ftp.storbinary('STOR ' + filename, file)
+    file.save(path)
+    logging.debug("  saved")
+    return path
 
-    logging.debug("  wrote file")
+
+def upload_file(ftp: FTP, filepath: pathlib.Path):
+    """Upload a file from a given path to the ftp."""
+    with open(filepath, 'rb') as file:
+        ftp.storbinary(f'STOR {filepath.name}', file)
+    logging.debug("  uploaded")
 
   
 @app.route('/')
 @requires_auth
 def main():  
     logging.debug("/ endpoint")
-    return render_template("Index.html") 
+    return render_template("Index.html")
 
 
 @app.route('/success', methods=['POST'])
@@ -93,22 +102,11 @@ def main():
 def upload():
     logging.debug("Upload received")
 
-    if request.authorization:
-        username = request.authorization.username
-        password = request.authorization.password
-    else:
-        username = request.values.get("user", "")
-        password = request.values.get("password", "")
-
     msg = "<h1>Uploaded Files</h1><ul>"
 
     try:
-
-        target_folder = request.values.get("folder", "")
-        user_name = request.values["user"]
-
-        user_name = secure_filename(user_name)
-        if target_folder:
+        user_name = secure_filename(request.values["user"])
+        if target_folder := request.values.get("folder", ""):
             target_folder = secure_filename(target_folder)
 
         logging.debug(f"target_folder {target_folder}")
@@ -119,47 +117,43 @@ def upload():
             if 'files' not in request.files:
                 return 'No files part of the request', 400
 
-            files = request.files.getlist('files')
+            files: List[FileStorage] = request.files.getlist('files')
 
             logging.debug("Logging into FTP server")
             ftp = FTP()
             ftp.connect(FTP_HOST, FTP_PORT)
-            ftp.login(username, password)
+            ftp.login(*get_credentials())
             logging.debug("  login successful")
 
-
-            chdir(ftp, user_name)
+            # create personal directory for current user
+            if not is_directory(ftp, user_name):
+                ftp.mkd(user_name)
+            ftp.cwd(user_name)
             logging.debug(f"  changed dir {user_name}")
+
+            # create subdirectory in personal user directory
             if target_folder:
-                chdir(ftp, target_folder)
+                if not is_directory(ftp, target_folder):
+                    ftp.mkd(target_folder)
+                ftp.cwd(target_folder)
                 logging.debug(f"  changed dir {target_folder}")
 
             for file in files:
                 try:
-
                     logging.debug(f"Processing file {file.filename}")
-                    if file.filename == '':
+                    if not file.filename:
                         raise ValueError('One or more files do not have a name (probably uploaded empty file)')
-                    
-                    file_name = secure_filename(file.filename)
 
-                    filepath = os.path.join(upload_dir, file_name)
+                    path = store_file(pathlib.Path(upload_dir), file)
+                    upload_file(ftp, path)
 
-                    file.save(filepath)
-                    logging.debug("  saved")
-                    upload_file_to_ftp(ftp, filepath, file_name)
-                    logging.debug("  uploaded")
-
-                    msg += f"<li>{file_name}: Success"
-
+                    msg += f"<li>{file.filename}: Success"
                 except Exception as e:
                     logging.debug(f"exception {e.__class__}: {e}")
                     msg += f"<li>{file.filename}: {e.__class__.__name__}: {e}"
 
     except Exception as e:
-
         logging.debug(f"{e.__class__}: {e}")
-
         return f"Something went wrong<br> {e.__class__}: {e}", 500
 
     msg += "</ul>"
